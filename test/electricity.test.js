72aac6e8695e8761ca231122b2283f29678cdfd3
Unalo <unalomtshokotsha@gmail.com>
2023-05-29 15:20:54 +0200

    added tets cases for advanced and paying off advanced taken

--- test/electricity.test.js
+++ test/electricity.test.js
@@ -34,15 +34,58 @@ describe("The buy electricity app", function () {
         assert.equal(35, electricity.getElectricity());
     })
 
-    // it("should get all the appliences", function () {
+    it("should allow a user to top-up electricity multiple times", function () {
 
-    //     let electricity = Electricity();
+        let electricity = Electricity();
 
-    //     assert.deepEqual(electricity.getAppliances(), [{
+        electricity.topUpElectricity(20);
+        electricity.topUpElectricity(10);
+        electricity.topUpElectricity(50);
+        electricity.topUpElectricity(20);
 
-    //     }]);
+        assert.equal(73, electricity.getElectricity());
+    })
 
-    // })
+    it("should not allow a user to take advanced more than once without paying the balance", function () {
+
+        let electricity = Electricity();
+
+        electricity.topUpElectricity('advanced');
+        electricity.topUpElectricity('advanced');
+
+        assert.equal(21, electricity.getElectricity());
+    })
+
+    it("should allow a user to take advanced and pay for the advanced", function () {
+
+        let electricity = Electricity();
+
+        electricity.topUpElectricity('advanced');
+        electricity.topUpElectricity(50);
+        electricity.topUpElectricity('advanced');
+
+        assert.equal(35, electricity.getElectricity());
+    })
+
+
+    it("should allow a user to take advanced and pay for the advanced and use appliances", function () {
+
+        let electricity = Electricity();
+
+        electricity.topUpElectricity('advanced');
+        electricity.topUpElectricity(20);
+
+        // advanced ignored as you still owe R10
+        electricity.topUpElectricity('advanced');
+        electricity.topUpElectricity(20);
+
+        electricity.useAppliance('Tv');
+
+        // advanced is valid now
+        electricity.topUpElectricity('advanced');
+
+        assert.equal(25, electricity.getElectricity());
+    })
 
     it("should allow appliences usage", function () {
 
