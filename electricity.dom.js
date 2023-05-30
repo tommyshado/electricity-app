411883a60a58af646f0c6425c9e7972452870e0e
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-30 09:50:54 +0200

    got reference to the radio button for the name 'buyElectricity'

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,16 @@
 // DOM element(s) references
 
+// radio button for top up electricity
+const topUpElectricity = document.querySelector("input[name='buyElectricity']");
 
+// buttons
+const buttonForBuying = document.querySelector('.btn');
 
 // Factory Function instance 
 const electricity =  Electricity();
 
 
 // DOM events here 
+buttonForBuying.addEventListener('click', function() {
+    if (topUpElectricity) {};
+});
